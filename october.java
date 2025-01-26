36dbb5723d4b671c5882fce0b4398f5647cc57d9
Rgandham45 <rahulgvr001@gmail.com>
2025-01-26 12:35:07 -0500

    improvedcode

--- october.java
+++ october.java
@@ -25,7 +25,77 @@ import java.util.Scanner;
                 int Original_num = num1;
                 int rev = 0;
                 while(num1!=0){
-                    int digit = num1 % 10;
+                    int digit = num1 % 10;package justforfun;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class October {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter option:");
+        System.out.println("1: Prime number");
+        System.out.println("2: Palindrome");
+        int option = getValidInput(scanner);
+        switch (option) {
+            case 1:
+                System.out.println("Enter a number to check if it's prime:");
+                int number = getValidInput(scanner);
+                if (isPrime(number)) {
+                    System.out.println(number + " is a prime number");
+                } else {
+                    System.out.println(number + " is not a prime number");
+                }
+                break;
+            case 2:
+                System.out.println("Enter a number to check if it's a palindrome:");
+                int palindromeNumber = getValidInput(scanner);
+                if (isPalindrome(palindromeNumber)) {
+                    System.out.println(palindromeNumber + " is a palindrome");
+                } else {
+                    System.out.println(palindromeNumber + " is not a palindrome");
+                }
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
+        scanner.close();
+    }
+
+    private static int getValidInput(Scanner scanner) {
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                scanner.next();
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        return originalNumber == reversedNumber;
+    }
+}
                     rev = rev * 10 + digit;
                     num1 = num1 / 10;
                 }
